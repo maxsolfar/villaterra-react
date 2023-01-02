8ce66fd6ee80b853cfe998296a1ddde428c111d2
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-01-01 23:12:27 -0500

    Add Router & Basic Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import {NavBar} from "./components";
 import { ForgotPassword, Home, Offers, Profile, SignIn, SignUp } from "./pages";
 
+
 function App() {
+  const [theme, setTheme] = useState(localStorage.theme || "light");
+
+  useEffect(()=>{
+    if (theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      localStorage.theme = 'dark';
+      document.documentElement.classList.add('dark');
+    } else {
+      localStorage.theme = 'light';
+      document.documentElement.classList.remove('dark');
+    }
+  },[theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
   return (
-    <main>
-      <NavBar />
+    <main className="bg-light dark:bg-dark">
+      <NavBar theme={theme} handleThemeSwitch={handleThemeSwitch} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
