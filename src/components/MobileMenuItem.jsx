650f4dbd98fcbc1726ec082ba0691f9d73a67945
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-01-02 15:29:01 -0500

    Add: Menu Mobile Component

---
+++ src/components/MobileMenuItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Disclosure } from '@headlessui/react';
+const MobileMenuItem = ({ title, url, status, setPage }) => {
+  return (
+    <>
+      {status ? (
+        <Disclosure.Button
+          as={Link}
+          to={url}
+          onClick={setPage}
+          className="block border-l-4 border-primary bg-principal-100 py-2 pl-3 pr-4 text-base font-medium text-terceary sm:pl-5 sm:pr-6"
+        >
+          {title}
+        </Disclosure.Button>
+      ) : (
+        <Disclosure.Button
+          as={Link}
+          to={url}
+          onClick={setPage}
+          className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+        >
+          {title}
+        </Disclosure.Button>
+      )}
+    </>
+  );
+};
+
+export default MobileMenuItem;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import NavBar from "./NavBar";
+import MenuItem from "./MenuItem";
+import MobileMenuItem from "./MobileMenuItem";
 
 export {
   NavBar,
+  MenuItem,
+  MobileMenuItem
 }
