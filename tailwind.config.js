b86267f7eecb48d79926964c6b521ca98e5dc1b0
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-01-01 23:12:06 -0500

    Add custom styles Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        principal: {
+          100: '#fff0eb',
+          200: '#FFC3AD',
+          300: '#FFA585',
+          400: '#FF9670',
+          500: '#FF7D4E',
+          600: '#9D0001',
+          700: '#680001',
+          800: '#4E0000',
+          900: '#340000',
+        },
+        secondarial: {
+          100: '#F7E6E6',
+          200: '#EBBFC0',
+          300: '#DF9999',
+          400: '#C64D4D',
+          500: '#003049',
+          600: '#9D0001',
+          700: '#680001',
+          800: '#4E0000',
+          900: '#340000',
+        },
+        primary: '#FF7D4E',
+        secondary: '#14284A',
+        terceary: '#345EA8',
+        dark: '#0F172A',
+        light: '#f9f9f9',
+      },
+    },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
