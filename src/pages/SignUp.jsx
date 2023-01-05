8d361a69be09cb430c202fe681fb38366a7f2e1c
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-01-05 16:41:56 -0500

    Feat: Add Sign Up Page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,195 @@
 import React from 'react';
+import { BsTwitter, BsFacebook } from 'react-icons/bs';
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-  return (<div>Sign Up</div>);
+  return (
+    <>
+      <div className="flex min-h-full h-[calc(100vh-64px)]">
+        <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-28">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <h2 className="mt-6 text-3xl font-bold tracking-tight text-secondary dark:text-gray-100">
+                Sign up to VillaTerra
+              </h2>
+              <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">
+                Or{' '}
+                <a
+                  href="/"
+                  className="font-medium text-primary-400 hover:text-primary-500"
+                >
+                  start your 14-day free trial
+                </a>
+              </p>
+            </div>
+
+            <div className="mt-8">
+              <div>
+                <div>
+                  <p className="text-sm font-medium text-gray-700 dark:text-gray-200">
+                    Sign up with
+                  </p>
+
+                  <div className="mt-1 grid grid-cols-3 gap-3">
+                    <div>
+                      <a
+                        href="/"
+                        className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-100"
+                      >
+                        <span className="sr-only">Sign up with Facebook</span>
+                        <BsFacebook className="text-xl" />
+                      </a>
+                    </div>
+
+                    <div>
+                      <a
+                        href="/"
+                        className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-100"
+                      >
+                        <span className="sr-only">Sign up with Twitter</span>
+                        <BsTwitter className="text-xl" />
+                      </a>
+                    </div>
+
+                    <div>
+                      <a
+                        href="/"
+                        className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-100"
+                      >
+                        <span className="sr-only">Sign up with Google</span>
+                        <AiOutlineGoogle className="text-xl" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="relative mt-6">
+                  <div
+                    className="absolute inset-0 flex items-center"
+                    aria-hidden="true"
+                  >
+                    <div className="w-full border-t border-gray-300" />
+                  </div>
+                  <div className="relative flex justify-center text-sm">
+                    <span className="bg-gray-100 px-3 py-1 text-secondary-400 dark:text-secondary-400 rounded shadow">
+                      Or continue with
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <form action="#" method="POST" className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                    >
+                      Full Name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        autoComplete="name"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                    >
+                      Email address
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="space-y-1">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="space-y-1">
+                    <label
+                      htmlFor="confirm-password"
+                      className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                    >
+                      Confirm Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="confirm-password"
+                        name="confirm-password"
+                        type="password"
+                        autoComplete="confirm-password"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md border border-transparent bg-primary-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+                    >
+                      Sign up
+                    </button>
+                  </div>
+                </form>
+                <div className="text-sm py-4 text-center">
+                  <span className="font-medium text-secondary dark:text-gray-50 mr-2">
+                    Already have an Account?
+                  </span>
+                  <Link
+                    to="/sign-in"
+                    className="font-semibold text-md text-primary-400 hover:text-primary-500"
+                  >
+                    Sign In
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            className="absolute inset-0 h-full w-full object-cover rounded-bl-[25%]"
+            src="https://images.unsplash.com/photo-1592595896551-12b371d546d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
