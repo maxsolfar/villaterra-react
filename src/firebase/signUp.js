f806fb632037e9e8537c6f08bec83e472e24ce60
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-01-07 00:58:36 -0500

    Feat: Refactor Firebase Config & SignUp

---
+++ src/firebase/signUp.js
@@ -0,0 +1,30 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import { db } from '../firebase/config';
+import { serverTimestamp, setDoc, doc } from "firebase/firestore";
+import { toast } from 'react-toastify';
+
+export const signUp = async (email, password, name, data) => {
+  try {
+    const auth = getAuth();
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+    const user = userCredentials.user;
+    const dataCopy = { ...data };
+    delete dataCopy.password;
+    dataCopy.timestamp = serverTimestamp();
+    await setDoc(doc(db, "users", user.uid), dataCopy);
+    toast.success("Created!");
+  } catch (error) {
+    toast.error("Something went wrong with the sign up");
+  }
+};
