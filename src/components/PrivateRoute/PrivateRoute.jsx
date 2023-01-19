0189638f3f041fd9b4bb53742163735c77209d95
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-01-18 21:10:13 -0500

    Add: Custon Hook Auth Status | closes #21

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useStatusAuth } from '../../hooks/useStatusAuth';
 
 const PrivateRoute = () => {
-  const loggedIn = true;
-  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />
-}
+  const { loggedIn, checkingStatus } = useStatusAuth();
+  if (checkingStatus) {
+    return (
+      <section className='w-full flex items-center justify-center'>
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#00C6BA"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      </section>
+    );
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+};
 
 export default PrivateRoute;
