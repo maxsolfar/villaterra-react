369fd6b97add5c9a4b2e44f69f5af9a9b95dfbba
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-02-12 23:02:34 -0500

    Modified: Add Dark Styles Label Switch Component

--- src/components/FormProfile/LabelSwitch.jsx
+++ src/components/FormProfile/LabelSwitch.jsx
@@ -6,11 +6,11 @@ const LabelSwitch = ({title, description, switchOption, onChange}) => {
     return classes.filter(Boolean).join(' ');
   }
   return (
-    <Switch.Group as="li" className="flex items-center justify-between py-4">
+    <Switch.Group as="li" className="flex items-center justify-between py-4 bg-light dark:bg-dark">
       <div className="flex flex-col">
         <Switch.Label
           as="p"
-          className="text-sm font-medium text-gray-900"
+          className="text-sm font-medium text-dark dark:text-light"
           passive
         >
           {title}
@@ -23,15 +23,15 @@ const LabelSwitch = ({title, description, switchOption, onChange}) => {
         checked={switchOption}
         onChange={onChange}
         className={classNames(
-          switchOption ? 'bg-teal-500' : 'bg-gray-200',
-          'relative ml-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2'
+          switchOption ? 'bg-primary-500' : 'bg-gray-200 dark:bg-gray-700',
+          'relative ml-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2'
         )}
       >
         <span
           aria-hidden="true"
           className={classNames(
             switchOption ? 'translate-x-5' : 'translate-x-0',
-            'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+            'inline-block h-5 w-5 transform rounded-full bg-white dark:bg-gray-800 shadow ring-0 transition duration-200 ease-in-out'
           )}
         />
       </Switch>
